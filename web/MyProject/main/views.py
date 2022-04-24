a0aa209e26990a3ad1d3a40bafa656cd8a61d36f
Єгор Грушевий <yegorgru@gmail.com>
2022-04-24 16:37:41 +0300

    add styles

--- web/MyProject/main/views.py
+++ web/MyProject/main/views.py
@@ -5,7 +5,8 @@ from .forms import UserForm
 
 
 def index(request):
-    form = UserForm(request.POST or None)
+    form = UserForm(request.POST or None, initial={'unit_id': 0})
+    id = 0
     if form.is_valid():
         id = form.cleaned_data.get("unit_id")
     data = list(Employee.objects.all().filter(unitId=int(id)))
